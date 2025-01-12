9f3144dc9bd7dfe78524ecd8518de20d85aff8a3
FEARxTHExGOOSE <joliverroberts@gmail.com>
2025-01-12 12:58:09 -0800

    Add files via upload

---
+++ CS 161 Assignment 1.py
@@ -0,0 +1,34 @@
+print ('Question 1')
+pet_type= "dog"
+pet_name= "Rosie"
+pet_gender= "her"
+print(f"I have a {pet_type} and {pet_gender} name is {pet_name}.")
+
+print ('Question 2')
+
+name= 'Alice'
+current_age= '20'
+yearly_savings= 1200
+future_age= '30'
+saving_years= 5
+monthly_savings= '$100.00'
+
+print(f'Hello {name}! You are currently {current_age} years old.')
+print(f'In 10 years, you will be {future_age} years old.')
+print(f'If you save ${yearly_savings} each year, in {saving_years} years you will have saved ${yearly_savings * saving_years}.')
+print(f'Your average monthly savings is {monthly_savings}.')
+
+print ('Question 3')
+
+days_month= 31
+seconds_day= 86400
+month= 'January'
+
+print(f'The number of seconds in {month} is {days_month*seconds_day}.')
+
+
+print ('Question 4')
+
+egg_amount= 37
+print(f'Enter the number of eggs:{egg_amount}')
+print(f'This makes {egg_amount//12} dozen eggs with {egg_amount%12} left over.')
